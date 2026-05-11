a7cc255a16e08e254550117dd203f1e1ce8e99ce
Ben Miller <ben@hyl.co.uk>
2026-05-11 17:01:43 +0100

    walking words

--- static/js/main.js
+++ static/js/main.js
@@ -51,7 +51,6 @@ const phrases = [
   "he yet laughs",
   "he yet lingered",
   "he yet lives",
-  "he yet loiters",
   "he yielded little",
   "he yields little",
   "he yields loyally",
@@ -113,54 +112,34 @@ const phrases = [
   "hide your loot",
   "high yelps linger",
   "hilarious yokels lark",
-  "hilarious yuppies lounge",
   "his yak leapt",
   "his yarn lengthened",
   "his yearning lasted",
   "his years lapsed",
-  "his years limited",
-  "his yelling lapsed",
-  "his yelling lingered",
-  "his yellowed lamp",
   "his yellowed letter",
   "his yellowing leaves",
-  "his yesterday lost",
   "his yodeling lulled",
-  "his yoke lifted",
   "his youngest leapt",
   "his youngest loved",
   "his youth lingers",
-  "his youth lost",
   "his youthful laugh",
-  "hissing yappers lunge",
-  "hissing yokels lurk",
-  "hissing yuppies lurch",
   "hoarse yellers lament",
   "hoist your ladder",
   "hold your limpet",
   "hold your llama",
   "hollow yachts list",
-  "hollow yearners languish",
   "hollow years last",
   "hollow years linger",
-  "hollow yelps linger",
-  "hollow yobs lurk",
+  "hateful yobs lurk",
   "hollow your log",
-  "hollow youth laments",
-  "honest yaks lope",
   "honest yearners live",
   "honest youth learns",
   "honestly you're lost",
   "honestly you're lucky",
-  "hopeful yarns lull",
   "hopeful years loom",
   "hopeful yodelers laugh",
-  "hopeful yogi lights",
   "hopeful youngsters listen",
-  "hopeless yaks lumber",
-  "hopeless yearners languish",
   "horses yawn lazily",
-  "hot yams lure",
   "hover you loon",
   "how years lapse",
   "how years linger",
@@ -178,21 +157,12 @@ const phrases = [
   "how you've lumbered",
   "how's your luck",
   "how youth lapses",
-  "howling yards liven",
-  "howling yellow lightning",
   "howling yetis lunge",
-  "howling younglings leap",
   "hug your llama",
-  "huge yachts loom",
   "huge yeomen labor",
   "hulking yetis lumber",
-  "humble yaks lumber",
-  "humble yearners learn",
-  "humble yelps linger",
-  "humble yodelers learn",
   "humble yogis live",
   "humid yards linger",
-  "hungry yabbos loot",
   "hungry yaks leap",
   "hungry yetis lurk",
   "hurl your lance",
@@ -203,12 +173,8 @@ const phrases = [
   "hurried yuppies lurch",
   "hurry you laggard",
   "hushed yearnings linger",
-  "hushed yellows lull",
   "hushed yodelers lament",
-  "husky yeomen labor",
   "husky yetis lumber",
-  "husky yodelers lament",
-  "hyper yabbos leap",
   "hyper younglings leap",
 ]
 
@@ -244,6 +210,9 @@ function runTile(tile, finalChar, totalCycles) {
 
 const phrase = phrases[Math.floor(Math.random() * phrases.length)]
 const h1 = document.querySelector('h1')
+const h1Link = document.createElement('a')
+h1Link.href = '/'
+h1.appendChild(h1Link)
 const boldSecond = Math.random() < 0.5
 
 let wordIndex = 0
@@ -254,7 +223,7 @@ phrase.split('').forEach((char, i) => {
   const isBold = !isSpace && (boldSecond ? wordIndex === 1 : wordIndex === 0 || wordIndex === 2)
   tile.className = isSpace ? 'tile tile--space' : isBold ? 'tile tile--bold' : 'tile'
   tile.textContent = isSpace ? '\u00A0' : randomChar()
-  h1.appendChild(tile)
+  h1Link.appendChild(tile)
 
   if (!isSpace) {
     const cycles = i + 3 + Math.floor(Math.random() * 4)
